5bb704b78ddaac6d462f5240b6037fc9dfc60523
Faisal N <narsi.faisal@gmail.com>
2017-04-16 22:33:34 -0500

    working on UI

---
+++ tiptuity/server.js
@@ -0,0 +1,25 @@
+
+var express = require("express");
+var exphbs = require("express-handlebars");
+var bodyParser = require("body-parser");
+var path = require("path");
+
+var PORT = process.env.PORT || 3000;
+var app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+app.use(express.static("app/public"));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
+
+app.get('/', function(req, res){
+  res.render('index');
+});
+
+app.listen(PORT);
